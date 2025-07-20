624fd45076d92d19cd1beb5e947b686f9d55b9e2
Moritz Schmidt <mo.schmidt1007@gmail.com>
2025-07-20 18:59:47 +0200

    new initial idea, console app to convert currently .stl to meshes and optimzing them

--- app/main.py
+++ app/main.py
@@ -1,44 +1,15 @@
-import FreeCAD
-from fem.geometry import GeometryManager
-from fem.boundary_conditions import BoundaryConditionManager
-from fem.solver import SolverManager
-from fem.results import ResultsManager
+import Mesh, Part, MeshPart
+import FreeCAD as App
 
 
-def main():
-    doc = FreeCAD.newDocument("TopOptModular")
+cyl = App.ActiveDocument.addObject("Part::Cylinder","Cylinder")
+App.ActiveDocument.recompute()
 
-    # Geometrie erstellen
-    box = doc.addObject("Part::Box", "Box")
-    box.Length = 100
-    box.Width = 50
-    box.Height = 10
-    doc.recompute()
-
-    # FEM Setup
-    geo = GeometryManager(doc)
-    analysis = geo.create_analysis()
-    geo.add_geometry(box)
-    geo.add_material()
-    mesh = geo.create_mesh()
-
-    # Randbedingungen
-    bc = BoundaryConditionManager(doc, analysis, box)
-    bc.add_fixed_constraint(face_ids=[1])
-    bc.add_force_constraint(face_ids=[2], force_value=1000)
-
-    # Solver
-    solver = SolverManager(doc, analysis)
-    solver.setup_calculix_solver()
-    solver.run_solver()
-
-    # Ergebnisse
-    results = ResultsManager(mesh, analysis)
-    coords, connectivity, _, _ = results.get_mesh_data()
-    displacements = results.get_displacement_field()
-
-    print("Displacements:", displacements)
+msh = App.ActiveDocument.addObject("Mesh::Feature", "Mesh")
+msh.Mesh = MeshPart.meshFromShape(Shape=cyl.Shape, MaxLength=1)
+msh.ViewObject.DisplayMode = "Flat Lines"
 
 
-if __name__ == "__main__":
-    main()
+new_mesh = msh.Mesh.copy()
+
+print(new_mesh)
--- utils/utils.py
+++
@@ -1,2 +0,0 @@
-def log(message):
-    print(f"[TopOpt] {message}")
