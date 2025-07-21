bb341447270dd0ede0a008c29efe5a1013a46e3e
Moritz Schmidt <mo.schmidt1007@gmail.com>
2025-07-21 07:22:43 +0200

    Backbone of FileHandling is made.

---
+++ source/Data/DataHandling/include/DataHandling/DataHandler.hpp
@@ -0,0 +1,17 @@
+#ifdef DataHandler
+#define DataHandler
+
+#pragma once
+// Includes
+
+// Namespace
+namespace TopologicOptimizer::Data
+{
+    class DataHandler{
+
+    };
+} // namespace TopologicOptimizer::Data
+
+
+
+#endif
